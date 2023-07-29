2d2e4599e1dcab61764c5d43bb8a36caa55b2325
NPThais <140864617+NPThais@users.noreply.github.com>
2023-07-29 08:25:30 +0700

    Update Githubtest.java

--- githubtest/src/githubtest/Githubtest.java
+++ githubtest/src/githubtest/Githubtest.java
@@ -16,8 +16,8 @@ public class Githubtest {
     public static void main(String[] args) {
         System.out.println("Hello word!");
         System.out.println("Hello word4");
-                System.out.println("Hello word!");
-        System.out.println("Hello word4");
+        System.out.println("Hello word!");
+        System.out.println("Hello kjawhduasdh");
         
     }
     
